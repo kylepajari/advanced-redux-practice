c639883742d1d2a73a307b6cd471e2e849d2f391
Kyle Pajari <kylepajari@yahoo.com>
2019-04-11 19:50:17 -0500

    missing commit

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+// const Reducer = (state = 0, action) => {
+//   switch (action) {
+//     case "INCREMENT":
+//       return state + 1;
+//     case "DECREMENT":
+//       return state - 1;
+//     default:
+//       return state;
+//   }
+// };
+
+const Reducer = (state, action) => {
+  return state;
+};
+
+export default Reducer;
